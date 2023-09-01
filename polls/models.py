518704e9fea0da2494195dc21955a972f7c0fe04
Nantawat SUKSIRISUNT <nantawat.suk@ku.th>
2023-09-01 21:32:48 +0700

    Code quality improves

--- polls/models.py
+++ polls/models.py
@@ -6,10 +6,16 @@ from django.contrib import admin
 
 
 class Question(models.Model):
+    """
+    Represents a question in the application.
+    """
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
+        """
+        Returns a string representation of the Question object.
+        """
         return self.question_text
 
     @admin.display(
@@ -18,14 +24,27 @@ class Question(models.Model):
         description='Published recently?',
     )
     def was_published_recently(self):
+        """
+        Checks if the question was published recently.
+
+        Returns:
+            bool: True if the question was published within the last day,
+                  False otherwise.
+        """
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
+    """
+    Represents a choice for a particular question in the application.
+    """
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
+        """
+        Returns a string representation of the Choice object.
+        """
         return self.choice_text
